f7c03d0ecd8332874ba8fa49b5810f599a07ad18
akparashar2002 <akashparashar1712@gmail.com>
2025-01-30 18:38:42 +0530

    updated

--- quix-app/index.js
+++ quix-app/index.js
@@ -2,5 +2,14 @@ function startQuiz(quizType) {
     alert(`Starting ${quizType.charAt(0).toUpperCase() + quizType.slice(1)} Quiz!`);
     
     // Redirecting to the corresponding quiz page based on the selected type
+
     window.location.href = `${quizType}-quiz.html`; // Assumes pages are named quiz-java.html, quiz-python.html, etc.
 }
+function Login(quizType) {
+   // alert(`Starting ${quizType.charAt(0).toUpperCase() + quizType.slice(1)} Quiz!`);
+    
+    // Redirecting to the corresponding quiz page based on the selected type
+
+    window.location.href = `${quizType}.html`; // Assumes pages are named quiz-java.html, quiz-python.html, etc.
+}
+
